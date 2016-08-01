1d776eb7454963432a75834bf98ffded6a8f3aa1
Mike Silberg <kochemirov@gmail.com>
2016-08-01 18:59:39 +0300

    JavaX first commit

---
+++ src/BicycleDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by mike on 7/27/16.
+ */
+public class BicycleDemo {
+
+    public static void main (String[] args) {
+        // create object
+        SpeedBicycle bike1 = new SpeedBicycle();
+        // set properties
+        bike1.setSpeed(18);
+        bike1.setPrice(4500);
+        // output results
+        bike1.printResult();
+    }
+
+}
---
+++ src/SpeedBicycle.java
@@ -0,0 +1,34 @@
+/**
+ * Created by mike on 7/27/16.
+ */
+
+interface Bicycle {
+
+    void setSpeed(int newSpeed);
+
+    void setPrice(int newPrice);
+
+    void printResult();
+
+}
+
+class SpeedBicycle implements Bicycle {
+
+    int speed;
+    int price;
+
+    @Override
+    public void setSpeed(int newSpeed) {
+        speed = newSpeed;
+    }
+
+    @Override
+    public void setPrice(int newPrice) {
+        price = newPrice;
+    }
+
+    @Override
+    public void printResult() {
+        System.out.println("bicycle has " + speed + " speeds, it costs " + price + " NIS");
+    }
+}
