71dac76e67f53eb7884abd3b61a8cf34ea8be435
Mike Silberg <kochemirov@gmail.com>
2016-08-31 18:31:59 +0300

    JavaX rv0.3.5

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,9 +1,5 @@
 import java.util.Arrays;
 
-/**
- * Created by mike on 31/08/16.
- */
-
 class ArrayDemo {
     private int[] fooArray = {100, 200, 300, 400, 500, 600, 700, 800, 900};
     private int[] fooArray2 = new int[3];
@@ -12,9 +8,12 @@ class ArrayDemo {
             {"four", "five", "six"},
             {"seven", "eight", "nine"}
     };
+    private char[] fooArray4 = {'a', 'b', 'c', 'd', 'e', 'f'};
 
     public void printResult() {
         System.arraycopy(fooArray, 0, fooArray2, 2, 1);
+        char[] fooCopyTo = Arrays.copyOfRange(fooArray4, 0, 5);
+
         System.out.println("==========================");
         System.out.println(Arrays.toString(fooArray));
         System.out.println("==========================");
@@ -22,6 +21,8 @@ class ArrayDemo {
         System.out.println("==========================");
         System.out.println(Arrays.deepToString(fooArray3));
         System.out.println("==========================");
-
+        System.out.println(Arrays.toString(fooArray4));
+        System.out.println("==========================");
+        System.out.println(Arrays.toString(fooCopyTo));
     }
 }
