f8b5081d78ff906e7eb55a37a5b645b2b01f7346
Mike Silberg <kochemirov@gmail.com>
2016-08-31 15:37:32 +0300

    JavaX rv0.3

--- src/SpeedBicycle.java
+++ src/SpeedBicycle.java
@@ -16,6 +16,10 @@ class SpeedBicycle implements Bicycle {
 
     private int speed;
     private int price;
+    private long idNumber = 123_456_789_012L;
+    private float pi = 3.14_15F;
+    private float foo = 5.25456F;
+    private int foo2 = 55;
 
     @Override
     public void setSpeed(int newSpeed) {
@@ -30,5 +34,9 @@ class SpeedBicycle implements Bicycle {
     @Override
     public void printResult() {
         System.out.println("bicycle has " + speed + " speeds, it costs " + price + " NIS");
+        System.out.println(idNumber);
+        System.out.println(pi);
+        System.out.println(foo);
+        System.out.println(foo2);
     }
 }
